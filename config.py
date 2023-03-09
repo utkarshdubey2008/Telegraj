0154a153e3e044f07516c6937a90325f3c420008
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2023-03-09 12:41:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,13 @@
 
-MUST_JOIN = "@TheAlphaBotz"
+import os
+
+# Telegram bot token
+TELEGRAM_BOT_TOKEN = os.environ.get('TELEGRAM_BOT_TOKEN')
+
+# Telegraph short name
+TELEGRAPH_SHORT_NAME = os.environ.get('TELEGRAPH_SHORT_NAME')
+
+# Set up Telegraph credentials
+telegraph_api = telegraph.api.Telegraph()
+telegraph_api.create_account(short_name=TELEGRAPH_SHORT_NAME)
+
