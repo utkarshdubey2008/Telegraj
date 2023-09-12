9fe0dabf216c32981643c6c4c7cf7bfaeb6eae4c
utkarshdubey2008 <97157730+utkarshdubey2008@users.noreply.github.com>
2023-09-12 11:55:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,24 +1,8 @@
-#    This file is part of the ChannelAutoForwarder distribution (https://github.com/utkarsh212646/Telegraph_uploader).
-#    Copyright (c) 2021 Rithunand
-#    
-#    This program is free software: you can redistribute it and/or modify  
-#    it under the terms of the GNU General Public License as published by  
-#    the Free Software Foundation, version 3.
-# 
-#    This program is distributed in the hope that it will be useful, but 
-#    WITHOUT ANY WARRANTY; without even the implied warranty of 
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
-#    General Public License for more details.
-# 
-#    License can be found in < https://github.com/utkarsh212646/Telegraph_Uploader/blob/main/License> 
-
-
-
 import telegraph
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 # Set up Telegraph credentials
-telegraph_api = telegraph.api.Telegraph()
+telegraph_api = telegraph.Telegraph()
 telegraph_api.create_account(short_name='your_short_name')
 
 # Define the function to handle the /start command
@@ -27,7 +11,7 @@ def start(update, context):
 
 # Define the function to handle file uploads
 def upload_file(update, context):
-    file = context.bot.getFile(update.message.document.file_id)
+    file = context.bot.get_file(update.message.document.file_id)
     file_url = telegraph_api.upload(file=file.file_path)[0]['url']
     update.message.reply_text(f'Your file has been uploaded to Telegraph: {file_url}')
 
@@ -41,3 +25,6 @@ dispatcher.add_handler(MessageHandler(Filters.document, upload_file))
 
 # Start the bot
 updater.start_polling()
+```
+
+Please make sure to replace `'your_short_name'` with your desired short name for Telegraph and `'your_bot_token'` with your actual Telegram bot token.
